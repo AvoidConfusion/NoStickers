be9321d3c8a5147468b55f63b9ae20657503e2a4
MBR#0001 <buildmachineubuntu@gmail.com>
2021-05-16 15:44:45 +0200

    update

--- index.js
+++ index.js
@@ -6,20 +6,8 @@ bot.login(process.env.TOKEN);
 
 bot.on("ready", () => console.log(bot.user.tag + " ready!"));
 
-bot.on("raw", packet => {
-    if (packet.t == "MESSAGE_CREATE" && "stickers" in packet.d && "guild_id" in packet.d) {
-        let channel = bot.guilds.cache.get(packet.d.guild_id)?.channels?.cache?.get?.(packet.d.channel_id);
-        if (!channel) return;
-
-        if (!channel.stickers) channel.stickers = [];
-        channel.stickers.push(packet.d.id);
-    }
-});
-
 bot.on("message", message => {
-    if (message.channel.stickers?.includes?.(message.id) && message.deletable) {
-        message.delete({reason: "stickers are haram"}).then(() => {
-            console.log("Deleted a message sent by " + message.author.tag + " containing a sticker in #" + message.channel.name + ", mashallah!");
-        }).catch(console.log);
+    if (message.stickers.size > 0 && message.deletable) {
+        message.delete({reason: "stickers are haram"}).catch(console.log);
     }
 });
